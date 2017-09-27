497e88e2b04efd8328b20bf4e71f6e9766c6d233
dylankinsella <dylan.kinsella@hidrmedia.co.uk>
2017-09-27 09:23:35 +0100

    adding space to arrow breadcrumbs

--- js/autoBreadcrumbs.js
+++ js/autoBreadcrumbs.js
@@ -26,7 +26,7 @@ function breadcrumbs() {
     for(y=1;y<x-i;y++){
       output += "../";
     }
-    output += bits[i] + "/\">" + bits[i] + "</a> »";
+    output += bits[i] + "/\">" + bits[i] + "</a> » ";
   }
   document.write(output);
   document.write("</div>");
